6a1efdd1b06fae584c9bb1d8d3e29bdfe06997c7
R. G. Esteves <rodolfo.g.esteves@intel.com>
2021-10-01 00:30:47 -0700

    Looks like calling the function works!

--- Program.cs
+++ Program.cs
@@ -11,7 +11,9 @@ namespace first
         static void Main(string[] args)
         {
             // There must be an easier way to do this
-            List<float> labelsList = Enumerable.Range(0, 13).Select(x => (float)x).ToList();
+            int dimensionality = 3;
+            int observations = 10;
+            List<float> labelsList = Enumerable.Range(0, dimensionality * observations).Select(x => (float)x).ToList();
 
 /*
             Console.WriteLine($"Hello World, looking at lib in [{libPath}]");
@@ -23,22 +25,25 @@ namespace first
             */
 
             Console.WriteLine($"Trying to call the external library.");
+            float[] labelsArray = labelsList.ToArray();
+            int result;
+            unsafe {
+                fixed(float* labelsPtr = &labelsArray[0]) {
+                    result = Native.createTable(labelsPtr, dimensionality, observations);                     
+                }
+            }
 
             Console.WriteLine($"The list contains [{labelsList.Count}] elements.");
-
+            Console.WriteLine($"The result of calling the native function is [{result}].");
             Console.WriteLine($"Done!!");
         }
 
+        const string libDirPath = @"/data/Documents/Snippets/onedal/first";
+        const string libPath = libDirPath + "/lib_function.so";
+
         internal static class Native
         {
-            /*
-            const string libDirPath = @"/data/Documents/Snippets/onedal/first";
-            const string libPath = libDirPath + "/lib_function.so";
-
-            [DllImport(libPath), EntryPoint="createTable"]
-            [DllImport(@"/data/Documents/Snippets/onedal/first/lib_function.so"), EntryPoint="createTable"]
-            */
-            [DllImport("/data/Documents/Snippets/onedal/first/lib_function.so")]
+            [DllImport(libPath, EntryPoint="createTable")]
             public unsafe static extern int createTable(float* data, int numFeatures, int numObservations);
         }
     }
