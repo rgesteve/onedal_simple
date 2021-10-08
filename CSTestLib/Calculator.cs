29ed0c451e7cd54119aa8d29a35c885a0722eb84
R. G. Esteves <rodolfo.g.esteves@intel.com>
2021-10-08 00:04:50 -0700

    Infrastructure for the C# interface

---
+++ CSTestLib/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace CSTestLib
+{
+  public class Calculator
+  {
+    public double Add(double a, double b)
+    {
+      return a * b;
+    }
+
+    public static void CSCreateTable()
+    {
+      // There must be an easier way to do this
+      int dimensionality = 3;
+      int observations = 10;
+      List<float> labelsList = Enumerable.Range(0, dimensionality * observations).Select(x => (float)x).ToList();
+
+      Console.WriteLine($"Hello World, looking at lib in [{libDirPath}]");
+      if (Directory.Exists(libDirPath)) {
+        Console.WriteLine("The path is there, should be OK");
+      } else {
+        Console.WriteLine("Can't find the path, there is a problem");
+      }
+
+      Console.WriteLine($"Trying to call the external library.");
+      float[] labelsArray = labelsList.ToArray();
+      int result;
+      unsafe {
+        fixed(float* labelsPtr = &labelsArray[0]) {
+          result = Native.createTable(labelsPtr, dimensionality, observations);                     
+        }
+      }
+
+      Console.WriteLine($"The list contains [{labelsList.Count}] elements.");
+      Console.WriteLine($"The result of calling the native function is [{result}].");
+      Console.WriteLine($"Done!!");
+    }
+
+    const string libDirPath = @"/data/Documents/Snippets/onedal/first/build";
+    const string libPath = libDirPath + "/libOneDALNative_lib.so";
+
+    internal static class Native
+    {
+      [DllImport(libPath, EntryPoint="createTable")]
+      public unsafe static extern int createTable(float* data, int numFeatures, int numObservations);
+    }
+  }
+}
---
+++ CSTestLibConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using CSTestLib;
+
+namespace CSTestLibConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var calc = new Calculator();
+            Console.WriteLine($"Calling library with 4 and 5: {calc.Add(4, 5)}...");
+            Calculator.CSCreateTable();
+            Console.WriteLine("Done!");
+        }
+    }
+}
