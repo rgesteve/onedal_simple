79c3598f71da5f1ac758a17bb6b2790d669fb139
R.G. Esteves <rgesteves@gmail.com>
2021-10-22 16:01:13 +0000

    Recovering model parameters from trained model

--- MLNetStandalone/Program.cs
+++ MLNetStandalone/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.ML;
+using Microsoft.ML.Trainers;
 using Microsoft.ML.Data;
 
 // docs/samples/Microsoft.ML.Samples/Dynamic/Trainers/Regression/OnlineGradientDescent.cs
@@ -35,9 +36,32 @@ namespace MLNetStandalone
 
         static void Main(string[] args)
         {
+            // Create a new context for ML.NET operations. It can be used for
+            // exception tracking and logging, as a catalog of available operations
+            // and as the source of randomness. Setting the seed to a fixed number
+            // in this example to make outputs deterministic.
+            var mlContext = new MLContext(seed: 0);
+
             // Create a list of training data points.
-            var dataPoints = GenerateRandomDataPoints(10);
+            var dataPoints = GenerateRandomDataPoints(100);
             Console.WriteLine($"Generated {dataPoints.Count()} data points.");
+
+            // Convert the list of data points to an IDataView object, which is
+            // consumable by ML.NET API.
+            var trainingData = mlContext.Data.LoadFromEnumerable(dataPoints);
+
+            // Define the trainer.
+            var pipeline = mlContext.Regression.Trainers.OnlineGradientDescent(
+                labelColumnName: nameof(DataPoint.Label),
+                featureColumnName: nameof(DataPoint.Features));
+
+            // Train the model.
+            var model = pipeline.Fit(trainingData);
+
+            LinearRegressionModelParameters modelParameters = ((ISingleFeaturePredictionTransformer<object>)model).Model as LinearRegressionModelParameters;
+
+            Console.WriteLine($"Done, the bias is: {modelParameters.Bias}, the weights are: [ {modelParameters.Weights.First()} ]!");
+
         }
     }
 }
