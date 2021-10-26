6b3d680bf8096f804c261b1993d4539442cfbb5a
R. G. Esteves <rodolfo.g.esteves@intel.com>
2021-10-25 22:44:01 -0700

    Super simple pure C# library to test in notebook

---
+++ CSPureLibSimple/Evaluator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSPureLibSimple
+{
+    public class Evaluator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
