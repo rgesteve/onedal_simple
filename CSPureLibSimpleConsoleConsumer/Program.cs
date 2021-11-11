d2ccd3bad6bfc9e80290c535b68665fee8eee510
R. G. Esteves <rgesteves@gmail.com>
2021-11-11 21:52:35 +0000

    A simple test of consuming a library through nupkg

---
+++ CSPureLibSimpleConsoleConsumer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CSPureLibSimple;
+
+namespace CSPureLibSimpleConsoleConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	    int testing = Evaluator.Add(3,4);
+            Console.WriteLine($"Result from library: [{testing}]");
+            Console.WriteLine("Done!");
+        }
+    }
+}
