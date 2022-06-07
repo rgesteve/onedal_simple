a04a7ff6b8bbd288854f806b94534731601e94cf
R. G. Esteves <rgesteves@gmail.com>
2022-06-06 20:48:28 -0700

    Aggregates data into array

--- CSTestLib/Calculator.cs
+++ CSTestLib/Calculator.cs
@@ -3,14 +3,180 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.Data.Analysis;
 
 namespace CSTestLib
 {
   public class Calculator
   {
+
+    public class KNNSampleDatapoint
+    {
+      [LoadColumn(0)]
+      public float f0 { get; set; }
+
+      [LoadColumn(1)]
+      public float f1 { get; set; }
+
+      [LoadColumn(2)]
+      public float f2 { get; set; }
+
+      [LoadColumn(3)]
+      public float f3 { get; set; }
+
+      [LoadColumn(4)]
+      public float f4 { get; set; }
+
+      [LoadColumn(5)]
+      public int Label { get; set; }
+    }
+
     public double Add(double a, double b)
     {
       return a * b;
+    }
+
+    private static void DisplaySpan<T>(string title, ReadOnlySpan<T> span) 
+    { 
+      Console.WriteLine(title); 
+      for (int i = 0; i < span.Length; i++) { 
+        Console.Write($"{span[i]}."); 
+      } 
+      Console.WriteLine(); 
+    }
+
+    private static void DisplayLinearArray<T>(string title, T[] array)
+    {
+      Console.WriteLine($"Displaying array of length [{array.Length}]: {title}");
+      for (int i = 0; i < array.Length; i++) {
+         Console.Write($"{array[i]}, ");
+      }
+      Console.WriteLine();
+    }
+
+    public static void ReadDataFilesUsingDF(string rootPath)
+    {
+    	var dataPath = Path.Join(rootPath, "k_nearest_neighbors_test.csv");
+	if (!File.Exists(dataPath)) {
+	  Console.WriteLine($"Cannot find specified path");
+	  return;
+	}
+	var dataFrame = DataFrame.LoadCsv(dataPath, header: false);
+	var idf = dataFrame as IDataView;
+	Console.WriteLine($"[ {idf.Schema} ]");
+
+	MLContext ctx = new MLContext();
+	var ppl = ctx.Transforms.Concatenate("Features", "Column0", "Column1", "Column2", "Column3", "Column4");
+	// transformed dataview
+	var tdv = ppl.Fit(idf).Transform(idf);
+	var featuresColumn = tdv.Schema["Features"];
+	var labelColumn = tdv.Schema["Column5"];
+	var feature0Column = tdv.Schema["Column0"];
+
+	//Console.WriteLine($"The type of aggregated features is {featuresColumn.Type}");
+
+	int samples = 0;
+	int maxSamples = 10;
+
+	int featureDimensionality = 5;  // should be able to get this from featuresColumn.Type
+	float[] data = new float[ maxSamples * featureDimensionality];
+	Span<float> dataSpan = new Span<float>(data);
+	Console.WriteLine($"Using an array of size {dataSpan.Length} to aggregate results");
+/*
+	//data.Clear();
+	*/
+
+	using (var cursor = tdv.GetRowCursor(new[] {feature0Column, featuresColumn, labelColumn})) {
+	  float f0Value = default;
+  	  VBuffer<float> featureValues = default(VBuffer<float>);
+
+	  var f0Getter = cursor.GetGetter<float>(feature0Column);
+  	  var featureGetter = cursor.GetGetter< VBuffer<float> >(featuresColumn);
+
+	  while (cursor.MoveNext() && samples < maxSamples) {
+	    f0Getter(ref f0Value);
+	    Console.Write($"row {samples}: {f0Value}");
+  	  
+	    featureGetter(ref featureValues);
+	    DisplaySpan<float>($"features at row {samples}: ", featureValues.GetValues());
+
+	    int offset = samples * featureDimensionality;
+
+	    /*
+	    Span<float> target = new Span<float>(dataSpan, offset, featureDimensionality);
+	    */
+	    Span<float> target = dataSpan.Slice(offset, featureDimensionality);
+	    featureValues.GetValues().CopyTo(target);
+	    /*
+	    float[] t = featureValues.GetValues().ToArray();
+	    for (int i = 0; i < featureDimensionality; i++) {
+	      Console.Write($"{t[i]}");
+	    }*/
+
+	    samples++;
+	  }
+	}
+	DisplayLinearArray("aggregate of data", data);
+    }
+
+    public static void ReadDataFiles(string rootPath)
+    {
+	var dataPath = Path.Join(rootPath, "k_nearest_neighbors_test.csv");
+	if (!File.Exists(dataPath)) {
+	  Console.WriteLine($"Cannot find specified path");
+	  return;
+	}
+	MLContext ctx = new MLContext();
+	int maxSamples = 10;
+	var dv = ctx.Data.LoadFromTextFile<KNNSampleDatapoint>(dataPath, hasHeader: false);
+	var ppl = ctx.Transforms.Concatenate("Features", "f0", "f1", "f2", "f3", "f4");
+	// transformed dataview
+	var tdv = ppl.Fit(dv).Transform(dv);
+	var featureColumn = tdv.Schema["Features"];
+	var labelColumn = tdv.Schema["Label"];
+
+	int samples = 0;
+	int featureDimensionality = 5;
+	float[] data = new float[ maxSamples * featureDimensionality];
+	Span<float> dataSpan = new Span<float>(data);
+	//data.Clear();
+
+	Console.WriteLine($"The accumulated buffer is of length [{data.Length}]");
+
+	using (var cursor = tdv.GetRowCursor(new[] {featureColumn, labelColumn})) {
+	  VBuffer<float> featureValues = default(VBuffer<float>);
+	  int labelValue = default;
+
+	  var featureGetter = cursor.GetGetter< VBuffer<float> >(featureColumn);
+  	  var labelGetter = cursor.GetGetter<int>(labelColumn);
+
+	  while (cursor.MoveNext() && samples < maxSamples) {
+	    featureGetter(ref featureValues);
+	    labelGetter(ref labelValue);
+	    int offset = samples * featureDimensionality;
+	    //Span<float> target = new Span<float>(dataSpan, offset, featureDimensionality);
+	    Span<float> target = dataSpan.Slice(offset, featureDimensionality);
+	    featureValues.GetValues().CopyTo(target);
+	    float[] t = featureValues.GetValues().ToArray();
+	    for (int i = 0; i < featureDimensionality; i++) {
+	      Console.Write($"{t[i]}");
+	    }
+	    Console.WriteLine($"--- {labelValue}");
+	    samples++;
+	    Console.WriteLine($"this features is {featureValues.IsDense} dense of {featureValues.Length} dimensionality.  Want to copy to ({offset} of length {featureValues.GetValues().ToString()})");
+	  }
+	}
+	Console.WriteLine($"Should be working: {featureColumn.Name} with type {featureColumn.Type.ToString()} (read {samples} samples)!");
+	float[] data1 = dataSpan.ToArray();
+	for (int i = 0; i < maxSamples; i++) {
+           Console.Write($"{i} : ");
+	   for (int j = 0; j < featureDimensionality; j++) {
+	      Console.Write($"{data1[i * featureDimensionality + j]}");
+	   }
+	   Console.WriteLine();
+	}
     }
 
     public static void CSCreateTable()
@@ -77,7 +243,8 @@ namespace CSTestLib
     const string libDirPath = @"C:\Users\rgesteve\Documents\projects\onedal_simple\build";
     const string libPath = libDirPath + @"\Debug\OneDALNative_lib.dll";
 #else
-    const string libDirPath = @"/data/Documents/Snippets/onedal/first/build";
+    //const string libDirPath = @"/data/Documents/Snippets/onedal/first/build";
+    const string libDirPath = @"/home/rgesteve/projects/onedal_simple/build";
     const string libPath = libDirPath + "/libOneDALNative_lib.so";
 #endif
 
