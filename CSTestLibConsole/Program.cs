05da1e34732cb75dcfa918a944996b88af5be22a
R. G. Esteves <rgesteves@gmail.com>
2022-06-07 06:36:49 -0700

    Added interface and ridiculous test function

--- CSTestLibConsole/Program.cs
+++ CSTestLibConsole/Program.cs
@@ -10,11 +10,24 @@ namespace CSTestLibConsole
         {
             Console.WriteLine("Hello World!");
 
+	    KNNClassifier knn = new KNNClassifier(5);
+	    Console.WriteLine($"Configured knn to have {knn.HowManyClasses()} classes.");
+
+/*
+	    var rootPath = Environment.GetEnvironmentVariable("DATAPATH");
+            Console.WriteLine($"trying with rootPath {rootPath}!");
+	    //Calculator.ReadDataFiles(rootPath);
+	    Calculator.ReadDataFilesUsingDF(rootPath);
+	    */
+
+/*
             var calc = new Calculator();
             Console.WriteLine($"Calling library with 4 and 5: {calc.Add(4, 5)}...");
             Calculator.CSCreateTable();
             Calculator.LinRegTrain(1, 100);
+	    	    */
             Console.WriteLine("Done!");
+
         }
     }
 }
