62dfed20daaee935ba537e06cfb3a72ef00f1aa8
R. G. Esteves <rgesteves@gmail.com>
2022-09-06 10:37:12 -0700

    A bunch of updates, including moving up to more recent frameworks

--- Native/build.sh
+++ Native/build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 BUILDDIR=build
+PUBLISHDIR=publish
 
 rm -rf ${BUILDDIR}
 
 mkdir -p ${BUILDDIR}
 cmake -S . -B ${BUILDDIR}
 cmake --build ${BUILDDIR}
+
+nuget pack TestNative.nuspec
