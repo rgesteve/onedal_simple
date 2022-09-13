ee46f8552a988b2c1787befb72e24734e2fdf3a5
R. G. Esteves <rgesteves@gmail.com>
2022-09-13 03:55:31 -0700

    Mirror of the ML.NET integration for easier testing

---
+++ xgboost_proto/WrappedXGDMatrix.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace XGBoostProto
+{
+    /// <summary>
+    /// Wrapper of DMatrix object of XGBoost
+    /// </summary>
+    internal sealed class DMatrix : IDisposable
+    {
+
+        private WrappedXGBoostInterface.SafeDMatrixHandle _handle;
+        public WrappedXGBoostInterface.SafeDMatrixHandle Handle => _handle;
+
+        /// <summary>
+        /// Create a <see cref="DMatrix"/> for storing training and prediction data under XGBoost framework.
+#if true
+        public unsafe DMatrix(double[] data, float[] labels = null)
+	{
+	  _handle = null;
+	}
+#else
+        public unsafe Dataset(double[][] sampleValuePerColumn,
+            int[][] sampleIndicesPerColumn,
+            int numCol,
+            int[] sampleNonZeroCntPerColumn,
+            int numSampleRow,
+            int numTotalRow,
+            string param, float[] labels, float[] weights = null, int[] groups = null)
+        {
+            _handle = null;
+
+            // Use GCHandle to pin the memory, avoid the memory relocation.
+            GCHandle[] gcValues = new GCHandle[numCol];
+            GCHandle[] gcIndices = new GCHandle[numCol];
+            try
+            {
+                double*[] ptrArrayValues = new double*[numCol];
+                int*[] ptrArrayIndices = new int*[numCol];
+                for (int i = 0; i < numCol; i++)
+                {
+                    gcValues[i] = GCHandle.Alloc(sampleValuePerColumn[i], GCHandleType.Pinned);
+                    ptrArrayValues[i] = (double*)gcValues[i].AddrOfPinnedObject().ToPointer();
+                    gcIndices[i] = GCHandle.Alloc(sampleIndicesPerColumn[i], GCHandleType.Pinned);
+                    ptrArrayIndices[i] = (int*)gcIndices[i].AddrOfPinnedObject().ToPointer();
+                }
+                fixed (double** ptrValues = ptrArrayValues)
+                fixed (int** ptrIndices = ptrArrayIndices)
+                {
+                    // Create container. Examples will pushed in later.
+                    LightGbmInterfaceUtils.Check(WrappedLightGbmInterface.DatasetCreateFromSampledColumn(
+                        (IntPtr)ptrValues, (IntPtr)ptrIndices, numCol, sampleNonZeroCntPerColumn, numSampleRow, numTotalRow,
+                        param, out _handle));
+                }
+            }
+            finally
+            {
+                for (int i = 0; i < numCol; i++)
+                {
+                    if (gcValues[i].IsAllocated)
+                        gcValues[i].Free();
+                    if (gcIndices[i].IsAllocated)
+                        gcIndices[i].Free();
+                }
+            }
+            // Before adding examples (i.e., feature vectors of the original data set), the original labels, weights, and groups are added.
+            SetLabel(labels);
+            SetWeights(weights);
+            SetGroup(groups);
+
+#if false
+            Contracts.Assert(GetNumCols() == numCol);
+            Contracts.Assert(GetNumRows() == numTotalRow);
+	    #endif
+        }
+
+        public Dataset(Dataset reference, int numTotalRow, float[] labels, float[] weights = null, int[] groups = null)
+        {
+            WrappedLightGbmInterface.SafeDataSetHandle refHandle = reference?.Handle;
+
+            LightGbmInterfaceUtils.Check(WrappedLightGbmInterface.DatasetCreateByReference(refHandle, numTotalRow, out _handle));
+
+            SetLabel(labels);
+            SetWeights(weights);
+            SetGroup(groups);
+        }
+#endif
+
+        public void Dispose()
+        {
+            _handle?.Dispose();
+            _handle = null;
+        }
+
+#if false
+        public int GetNumRows()
+        {
+            int res = 0;
+            LightGbmInterfaceUtils.Check(WrappedLightGbmInterface.DatasetGetNumData(_handle, ref res));
+            return res;
+        }
+
+        public int GetNumCols()
+        {
+            int res = 0;
+            LightGbmInterfaceUtils.Check(WrappedLightGbmInterface.DatasetGetNumFeature(_handle, ref res));
+            return res;
+        }
+#endif
+
+        public unsafe void SetLabel(float[] labels)
+        {
+	#if false
+            Contracts.AssertValue(labels);
+            Contracts.Assert(labels.Length == GetNumRows());
+            fixed (float* ptr = labels)
+                LightGbmInterfaceUtils.Check(WrappedLightGbmInterface.DatasetSetField(_handle, "label", (IntPtr)ptr, labels.Length,
+                    WrappedLightGbmInterface.CApiDType.Float32));
+#endif
+        }
+    }
+}
---
+++ xgboost_proto/XGBoostBinaryClassifierEstimator.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#if false
+namespace Microsoft.ML.Trainers.XGBoost
+{
+
+    public sealed class XGBoostBinaryClassificationTransformer : OneToOneTransformerBase
+    {
+        internal XGBoostBinaryClassificationTransformer(IHost host, params (string outputColumnName, string inputColumnName)[] columns) : base(host, columns)
+        {
+        }
+
+        internal XGBoostBinaryClassificationTransformer(IHost host, ModelLoadContext ctx) : base(host, ctx)
+        {
+        }
+
+        private protected override IRowMapper MakeRowMapper(DataViewSchema schema) => new Mapper(this, schema);
+
+        private protected override void SaveModel(ModelSaveContext ctx)
+        {
+            Host.CheckValue(ctx, nameof(ctx));
+        }
+
+        private sealed class Mapper : OneToOneMapperBase, ISaveAsOnnx
+        {
+            private readonly XGBoostBinaryClassificationTransformer _parent;
+            public Mapper(XGBoostBinaryClassificationTransformer parent, DataViewSchema inputSchema)
+                : base(parent.Host.Register(nameof(Mapper)), parent, inputSchema)
+            {
+                _parent = parent;
+
+            }
+
+            public bool CanSaveOnnx(OnnxContext ctx) => true;
+
+            public void SaveAsOnnx(OnnxContext ctx)
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override DataViewSchema.DetachedColumn[] GetOutputColumnsCore()
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override Delegate MakeGetter(DataViewRow input, int iinfo, Func<int, bool> activeOutput, out Action disposer)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+#if false
+    public sealed class XGBoostBinaryClassificationEstimator : TrivialEstimator<XGBoostBinaryClassificationTransformer>
+#else
+    public sealed class XGBoostBinaryClassificationEstimator : IEstimator<XGBoostBinaryClassificationTransformer>
+#endif
+    {
+        private readonly IHost _host;
+
+
+       public sealed class Options : TrainerInputBase
+       {
+            /// <summary>
+            /// Maximum tree depth for base learners
+            /// </summary>
+            [Argument(ArgumentType.AtMostOnce, HelpText = "Maximum tree depth for base learners.", ShortName = "us")]
+            public int MaxDepth = 3;
+       }
+
+        public XGBoostBinaryClassificationEstimator(IHost host, XGBoostBinaryClassificationTransformer transformer) /*: base(host, transformer)*/
+        {
+            _host = Contracts.CheckRef(host, nameof(host)).Register(nameof(XGBoostBinaryClassificationEstimator));
+        }
+
+        public XGBoostBinaryClassificationTransformer Fit(IDataView input)
+        {
+	#if false
+            throw new NotImplementedException();
+	    #else
+	    	var featuresColumn = input.Schema["Features"];
+		var labelColumn = input.Schema["Label"];
+		int featureDimensionality = default(int);
+		if (featuresColumn.Type is VectorDataViewType vt) {
+		  featureDimensionality = vt.Size;
+		} else {
+		  _host.Except($"A vector input is expected");
+		}
+		int samples = 0;
+		int maxSamples = 10000;
+
+		float[] data = new float[ maxSamples * featureDimensionality];
+		float[] dataLabels = new float[ maxSamples ];
+		Span<float> dataSpan = new Span<float>(data);
+
+		using (var cursor = input.GetRowCursor(new[] { featuresColumn, labelColumn })) {
+
+		  float labelValue = default;
+		  VBuffer<float> featureValues = default(VBuffer<float>);
+
+		  var featureGetter = cursor.GetGetter< VBuffer<float> >(featuresColumn);
+		  var labelGetter = cursor.GetGetter<float>(labelColumn);
+
+		  while (cursor.MoveNext() && samples < maxSamples) {
+		    featureGetter(ref featureValues);
+		    labelGetter(ref labelValue);
+		    // DisplaySpan<float>($"features at row {samples}: ", featureValues.GetValues());
+
+		    int offset = samples * featureDimensionality;
+		    // Span<float> target = new Span<float>(dataSpan, offset, featureDimensionality);
+		    Span<float> target = dataSpan.Slice(offset, featureDimensionality);
+		    featureValues.GetValues().CopyTo(target);
+		    dataLabels[samples] = labelValue;
+		    	    	    #if false  	  
+		    samples++;
+		    #endif
+	    	  }
+	  	}
+
+		return new XGBoostBinaryClassificationTransformer(_host, ("Features", "PredictedLabel"));
+		#endif
+        }
+
+#if true
+        public SchemaShape GetOutputSchema(SchemaShape inputSchema)
+        {
+            throw new NotImplementedException();
+        }
+#else
+        // Used for schema propagation and verification in a pipeline (i.e., in an Estimator chain).
+        public override SchemaShape GetOutputSchema(SchemaShape inputSchema)
+        {
+            _host.CheckValue(inputSchema, nameof(inputSchema));
+            return new SchemaShape(inputSchema);
+        }
+#endif
+    }
+}
+#endif
