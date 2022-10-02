00258cbf55da14333d3cfd416e11a3e497713489
R. G. Esteves <rgesteves@gmail.com>
2022-10-02 00:47:43 -0700

    Little project to try cpack/nuget -- based on Ch4 of 'CMake best practices'

---
+++ NativeSimple/include/lib.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+/**
+ * @brief The 'greeter' class interface
+ */
+class greeter {
+public:
+  /**
+   * Greet the caller.
+   */
+  void greet();
+}; // class greeter
---
+++ NativeSimple/src/lib.cpp
@@ -0,0 +1,8 @@
+#include <lib.hpp>
+#include <iostream>
+
+void greeter::greet()
+{
+  std::cout << "Hello, world!" << std::endl;
+}
+
