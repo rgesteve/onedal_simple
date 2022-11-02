bb03e849342c585c1a55d300befbb815b5757fc3
R. G. Esteves <rodolfo.g.esteves@intel.com>
2022-11-02 01:50:21 -0700

    Added a native library that doesn't use onedal

--- Native/build.ps1
+++ Native/build.ps1
@@ -2,5 +2,5 @@ $builddir="build"
 
 Remove-Item -Path $builddir -Recurse 
 New-Item -Path $builddir -Type Directory 
-cmake -S . -B $builddir
+cmake -S . -B $builddir -DWITH_ONEDAL=NO
 cmake --build $builddir
